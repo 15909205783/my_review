befafb8542890c4b966b4b96882710a7d5c1bc32
yangwuhai <yangwuhai@zhuanzhuan.com>
2022-02-18 15:25:38 +0800

    @value注解是否能处理bolean值

---
+++ src/test/java/com/yangfan/neo/common/MyTest.java
@@ -0,0 +1,22 @@
+package com.yangfan.neo.common;
+
+import com.yangfan.neo.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class MyTest {
+    @Value("${yangfan.mytest}")
+    private boolean isY;
+
+    @Test
+    public void test(){
+        if (!isY){
+            System.out.println(isY);
+        }
+    }
+}
