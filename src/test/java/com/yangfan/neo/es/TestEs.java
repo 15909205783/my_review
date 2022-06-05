a33049f305349a75cada6352351a8bdcc778f556
15909205783 <2585928225@qq.com>
2022-06-05 23:47:19 +0800

    es

---
+++ src/test/java/com/yangfan/neo/es/TestEs.java
@@ -0,0 +1,85 @@
+package com.yangfan.neo.es;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yangfan.neo.Application;
+import org.apache.http.HttpHost;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TestEs {
+    private RestClient restClient;
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    @PostConstruct
+    public void init(){
+        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost("39.108.6.88", 9200, "http"));
+        this.restClient = restClientBuilder.build();
+    }
+    @Test
+    public void testGetInfo() throws IOException {
+        Request request = new Request("GET", "/_cluster/state");
+        request.addParameter("pretty", "true");
+        Response response = this.restClient.performRequest(request);
+        System.out.println(response.getStatusLine());
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    /**
+     * 根据ID查询数据
+     * @throws IOException
+     */
+    @Test
+    public void testGetHouseInfo() throws IOException {
+        Request request = new Request("GET", "/haoke/house/k1KTFYEBQtLd7ogV3GIG");
+        request.addParameter("pretty", "true");
+        Response response = this.restClient.performRequest(request);
+        System.out.println(response.getStatusLine());
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+
+    @Test
+    public void testCreateData() throws IOException {
+        Request request = new Request("POST", "/haoke/house");
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", "2001");
+        data.put("title", "张江高科");
+        data.put("price", "3500");
+        // 写成JSON
+        request.setJsonEntity(MAPPER.writeValueAsString(data));
+        Response response = this.restClient.performRequest(request);
+        System.out.println(response.getStatusLine());
+        System.out.println(EntityUtils.toString(response.getEntity()));
+
+
+    }
+
+    // 搜索数据
+    @Test
+    public void testSearchData() throws IOException {
+        Request request = new Request("POST", "/haoke/house/_search");
+        String searchJson = "{\"query\": {\"match\": {\"title\": \"拎包入住\"}}}";
+        request.setJsonEntity(searchJson);
+        request.addParameter("pretty","true");
+        Response response = this.restClient.performRequest(request);
+        System.out.println(response.getStatusLine());
+        System.out.println(EntityUtils.toString(response.getEntity()));
+
+    }
+
+
+}
